8a11bbb020e6c58bc5ab0023c19173a769d5d521
vibhor1997a <vibhor349a@rediffmail.com>
2017-12-21 00:02:54 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const version = require('./package.json').version;
 const cheerio = require('cheerio');
 const os = require('os');
 const homedir = os.userInfo().homedir;
-let dataDir = homedir + '\\cocodownloader';
+let solutionsDir = homedir + '\\cocodownloader';
 let getSolved = require('./lib/getsolved');
 let getSolutions = require('./lib/getsolutions');
 
@@ -18,16 +18,24 @@ commander.arguments('<username>').version(version).option('-v, --version', "outp
 }).action(update).parse(process.argv);
 
 function update(username) {
-    fs.exists(dataDir, (exists) => {
+    fs.exists(solutionsDir, (exists) => {
         if (!exists) {
-            fs.mkdirSync(dataDir);
+            fs.mkdirSync(solutionsDir);
         }
-        let userDir = dataDir + '\\' + username;
+        let userDir = solutionsDir + '\\' + username;
         if (!fs.existsSync(userDir)) {
             fs.mkdirSync(userDir);
         }
         getSolved(username, (err, problems) => {
-            
+            let p = [];
+            problems.forEach((problem) => {
+                p.push(getSolutions(problem));
+            });
+            Promise.all(p).then((solutions) => {
+                solutions = solutions.filter((x) => x.link != '');
+            }, (err) => {
+                console.log(err);
+            });
         });
     });
 }
